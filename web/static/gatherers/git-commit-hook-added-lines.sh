e5d19b7f4ed2ca2d96ab83bf69912b300554df5a
Julian Squires <julian@cipht.net>
2009-11-12 14:45:27 -0500

    Put the two current gatherers up on the website.

---
+++ web/static/gatherers/git-commit-hook-added-lines.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+host=`hostname`
+directory=`pwd`
+code=`cat ~/.level-up-code`
+metric="git commit $host:$directory"
+unit="lines of code"
+ratio="1:1"
+#url="http://localhost:8080/metric"
+url="https://level-up.appspot.com/metric"
+
+
+count=`git show -1 HEAD | grep '^\+' | wc -l`
+curl -F 'code='"$code" \
+     -F 'metric='"$metric" \
+     -F 'ratio='"$ratio" \
+     -F "value=$count" -F "unit=$unit" $url
